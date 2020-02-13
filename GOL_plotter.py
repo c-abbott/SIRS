1151da6f7eed80cf214bb73fb334d1901bad11f3
c-abbott <callum.abbott@outlook.com>
2020-02-13 19:01:13 +0000

    Added comments.

--- GOL_plotter.py
+++ GOL_plotter.py
@@ -19,40 +19,48 @@ def main():
         simulations = int(items[0]) # No. of simulations.
         ini_cond = str(items[1])    # Initial conditions.
         lattice_size = (int(items[2]), int(items[2]))  # Lattice size.
-        num_bins = int(items[3])
+        num_bins = int(items[3])    # Histogram bins.
 
     game = GOL(size=lattice_size, ini=ini_cond)
 
+    # Simulation for GOL steady state determination.
     if game.ini == 'random':
         eqm_times = []
         for i in range(simulations):
-            #print(i)
             live_cells = []
             game = GOL(size=lattice_size, ini=ini_cond)
             while game.eqm == False:
                 game.evolve_state()
+                # Count number of live cells in state.
                 live_cells.append(game.count_live())
-                if len(live_cells) > 3:
-                    if live_cells[len(live_cells)-1] == live_cells[len(live_cells)-2] and live_cells[len(live_cells)-2] == live_cells[len(live_cells)-3]:
-                        game.eqm = True
+                # Steady state determination.
+                game.check_eqm(live_cells)
+            # Minus 3 to account for check_eqm.
             eqm_times.append(len(live_cells) - 3)
-        game.plot_hist(eqm_times,num_bins)
+        # Plotting.
+        game.plot_hist(eqm_times, num_bins)
     
     elif game.ini == 'glider':
+        # Initialising data storage.
         x_pos = []
         y_pos = []
         times = []
         for i in range(simulations):
             for j in range(10):
                 game.evolve_state()
+            # Find live cells of glider.
             xs = game.get_glider_pos()[0]
             ys = game.get_glider_pos()[1]
+            # Check if at lattice boundary.
             x_checker, y_checker = game.boundary_checker(xs, ys)
             
+            # Store COM pos if not at lattice boundary.
             if x_checker == False and y_checker == False:
                 times.append(i)
                 x_pos.append(game.get_com(xs, ys)[0])
                 y_pos.append(game.get_com(xs, ys)[1])
-        game.plot_traj(times, x_pos)
+        # Printing glider velocity.
+        vel = game.plot_traj(times, x_pos, all = True)
+        print("The velocity of the glider is " + str(vel) + " cells / sweep")
 
 main()
