5b146423c35ca79fb6bf6b9a7b1474935328474a
c-abbott <callum.abbott@outlook.com>
2020-02-20 19:20:19 +0000

    Completed phase logic, plotting and writing.

--- SIRS_plotter.py
+++ SIRS_plotter.py
@@ -22,31 +22,37 @@ def main():
         eqm_sweeps = int(items[4]) # Equilibrium sweeps.
         sweeps = int(items[5])     # No. of sweeps.
 
-        # Initialising probability domains.
-        p1s = np.arange(0.0, 1.0, p_step)
-        p3s = np.arange(0.0, 1.0, p_step)
+    # Initialising probability domains.
+    p1s = np.arange(0.0, 1.0, p_step)
+    p3s = np.arange(0.0, 1.0, p_step)
+    # Initialising phase matrix.
+    phase_matrix = np.zeros((p1s.size, p3s.size))
 
-        matrix = np.zeros((p1s.size, p3s.size))
-
-        # Simulation begins.
-        for p1 in p1s:
-            print(p1)
-            # Data storage.
-            psi_per_p1 = []
-            for p3 in p3s:
-                simulation = SIRS(size=lattice_size,
-                                  ini=ini_cond, p1=p1, p2=p2, p3=p3)
-                psi_per_p3 = []
-                for sweep in range(sweeps):
-                    print(sweep)
-                    for j in range(simulation.size[0]*simulation.size[1]):
-                        simulation.update_SIRS()
-                    if sweep >= eqm_sweeps:
-                        psi_per_p3.append(simulation.get_infected_frac())
-
-                psi_per_p1.append(simulation.get_avg_obs(psi_per_p3))
-            matrix[:, p1] = psi_per_p1[::-1]
-        print(matrix)
+    # Simulation begins.
+    for p1 in p1s:
+        print(p1)
+        # Data storage.
+        psi_per_p1 = []
+        for p3 in p3s:
+            simulation = SIRS(size=lattice_size,
+                                ini=ini_cond, p1=p1, p2=p2, p3=p3)
+            psi_per_p3 = []
+            # Sweep over lattice.
+            for sweep in range(sweeps):
+                for j in range(simulation.size[0]*simulation.size[1]):
+                    simulation.update_SIRS()
+                if sweep >= eqm_sweeps:
+                    psi_per_p3.append(simulation.get_infected_frac())
+            # Data collection.
+            psi_per_p1.append(simulation.get_avg_obs(psi_per_p3))
+        # Update matrix columns.
+        phase_matrix[:, int(p1*10)] = psi_per_p1[::-1]
+    # Plotting.
+    simulation.plot_phase_diagram(phase_matrix)
+    
+    # Writing to file.
+    np.savetxt("phase_data.dat", phase_matrix, fmt='%1.5f', delimiter=' ')
 main()
+
 
         
