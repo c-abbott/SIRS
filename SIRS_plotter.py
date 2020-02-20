4cd06278414a8aca053090a01d76d01fb712c962
c-abbott <callum.abbott@outlook.com>
2020-02-20 17:19:24 +0000

    Calculated matrix for phase diagram

---
+++ SIRS_plotter.py
@@ -0,0 +1,52 @@
+from SIRS import SIRS
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+def main():
+    if len(sys.argv) != 2:
+        print("Wrong number of arguments.")
+        print("Usage: " + sys.argv[0] + " <parameters file>")
+        quit()
+    else:
+        infile_parameters = sys.argv[1]
+
+    # Open input file and assinging parameters.
+    with open(infile_parameters, "r") as input_file:
+        # Read the lines of the input data file.
+        line = input_file.readline()
+        items = line.split(", ")
+        lattice_size = (int(items[0]), int(items[0]))  # Lattice size.
+        ini_cond = str(items[1])   # Initial conditions.
+        p2 = float(items[2])       # P(I --> R).
+        p_step = float(items[3])   # Probability steps.
+        eqm_sweeps = int(items[4]) # Equilibrium sweeps.
+        sweeps = int(items[5])     # No. of sweeps.
+
+        # Initialising probability domains.
+        p1s = np.arange(0.0, 1.0, p_step)
+        p3s = np.arange(0.0, 1.0, p_step)
+
+        matrix = np.zeros((p1s.size, p3s.size))
+
+        # Simulation begins.
+        for p1 in p1s:
+            print(p1)
+            # Data storage.
+            psi_per_p1 = []
+            for p3 in p3s:
+                simulation = SIRS(size=lattice_size,
+                                  ini=ini_cond, p1=p1, p2=p2, p3=p3)
+                psi_per_p3 = []
+                for sweep in range(sweeps):
+                    print(sweep)
+                    for j in range(simulation.size[0]*simulation.size[1]):
+                        simulation.update_SIRS()
+                    if sweep >= eqm_sweeps:
+                        psi_per_p3.append(simulation.get_infected_frac())
+
+                psi_per_p1.append(simulation.get_avg_obs(psi_per_p3))
+            matrix[:, p1] = psi_per_p1[::-1]
+        print(matrix)
+main()
+
+        
