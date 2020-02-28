8562674a7e59f0b8ec3be24ea9c8681b04df7f7c
c-abbott <callum.abbott@outlook.com>
2020-02-28 11:59:26 +0000

    Added variance plot implementation.

--- SIRS_plotter.py
+++ SIRS_plotter.py
@@ -15,59 +15,84 @@ def main():
         # Read the lines of the input data file.
         line = input_file.readline()
         items = line.split(", ")
-        lattice_size = (int(items[0]), int(items[0]))  # Lattice size.
-        ini_cond = str(items[1])   # Initial conditions.
-        p2 = float(items[2])       # P(I --> R).
-        p_step = float(items[3])   # Probability steps.
-        eqm_sweeps = int(items[4]) # Equilibrium sweeps.
-        sweeps = int(items[5])     # No. of sweeps.
+        # Lattice size.
+        lattice_size = (int(items[0]), int(items[0])) 
+        desired_plot = str(items[1]) # Desired plot.
+        ini_cond = str(items[2])     # Initial conditions.
+        p2 = float(items[3])         # P(I --> R).
+        p_step = float(items[4])     # Probability steps.
+        eqm_sweeps = int(items[5])   # Equilibrium sweeps.
+        sweeps = int(items[6])       # No. of sweeps.
 
-    # Initialising probability domains.
-    p1s = np.arange(0.0, 1.0 + p_step, p_step)
-    #print(p1s.size)
-    p3s = np.arange(0.0, 1.0 + p_step, p_step)
-    # Initialising phase matrix.
-    phase_matrix = np.zeros((p1s.size, p3s.size))
-    var_matrix = np.zeros((p1s.size, p3s.size))
+    if desired_plot == 'heatmap':
+        # Initialising probability domains.
+        p1s = np.arange(0.0, 1.0 + p_step, p_step)
+        #print(p1s.size)
+        p3s = np.arange(0.0, 1.0 + p_step, p_step)
+        # Initialising phase matrix.
+        phase_matrix = np.zeros((p1s.size, p3s.size))
+        var_matrix = np.zeros((p1s.size, p3s.size))
 
-    # Simulation begins.
-    for p1 in p1s:
-        print(p1)
+        # Simulation begins.
+        for p1 in p1s:
+            print(p1)
+            # Data storage.
+            psi_per_p1 = []
+            var_data = []
+            for p3 in p3s:
+                simulation = SIRS(size=lattice_size,
+                                    ini=ini_cond, p1=p1, p2=p2, p3=p3)
+                psi_per_p3 = []
+                # Sweep over lattice.
+                for sweep in range(sweeps):
+                    for j in range(simulation.size[0]*simulation.size[1]):
+                        simulation.update_SIRS()
+                    if sweep >= eqm_sweeps:
+                        psi_per_p3.append(simulation.get_infected_frac())
+                        #psi = simulation.get_infected_frac()
+                        #if psi != 0:
+                        #    psi_per_p3.append(psi)
+                        #else:
+                        #    break
+                # Data collection.
+                psi_per_p1.append(simulation.get_avg_obs(
+                    psi_per_p3) / (simulation.size[0]*simulation.size[1]))
+                var_data.append(simulation.get_infected_var(
+                    psi_per_p3) / (simulation.size[0]*simulation.size[1]))
+            # Update matrix columns.
+            phase_matrix[:, int(p1*(p1s.size-1))] = psi_per_p1
+            var_matrix[:, int(p1*(p1s.size-1))] = var_data
+        
+        # Plotting.
+        simulation.plot_phase_diagram(phase_matrix, p_step)
+        simulation.plot_variance_contour(var_matrix, p_step)
+        
+        # Writing to file.
+        np.savetxt("phase_data.dat", phase_matrix, fmt='%1.5f', delimiter=' ')
+        np.savetxt("var_data.dat", var_matrix, fmt='%1.5f', delimiter=' ')
+    
+    elif desired_plot == 'variance_plot':
+        # Initialising probability domains.
+        p1s = np.arange(0.0, 1.0 + p_step, p_step)
+        p3 = 0.5
         # Data storage.
-        psi_per_p1 = []
-        var_data = []
-        for p3 in p3s:
+        var_array = np.zeros(p1s.size)
+        # Simulation begins.
+        for p1 in p1s:
+            print(p1)
+            psis = []
             simulation = SIRS(size=lattice_size,
-                                ini=ini_cond, p1=p1, p2=p2, p3=p3)
-            psi_per_p3 = []
-            # Sweep over lattice.
-            for sweep in range(sweeps):
+                              ini=ini_cond, p1=p1, p2=p2, p3=p3)
+            for sweep in range(sweeps * 10):
                 for j in range(simulation.size[0]*simulation.size[1]):
-                    simulation.update_SIRS()
+                        simulation.update_SIRS()
                 if sweep >= eqm_sweeps:
-                    psi_per_p3.append(simulation.get_infected_frac())
-                    #psi = simulation.get_infected_frac()
-                    #if psi != 0:
-                    #    psi_per_p3.append(psi)
-                    #else:
-                    #    break
-            # Data collection.
-            psi_per_p1.append(simulation.get_avg_obs(
-                psi_per_p3) / (simulation.size[0]*simulation.size[1]))
-            var_data.append(simulation.get_infected_var(
-                psi_per_p3) / (simulation.size[0]*simulation.size[1]))
-        # Update matrix columns.
-        phase_matrix[:, int(p1*(p1s.size-1))] = psi_per_p1
-        var_matrix[:, int(p1*(p1s.size-1))] = var_data
-    
-    # Plotting.
-    simulation.plot_phase_diagram(phase_matrix, p_step)
-    simulation.plot_variance_contour(var_matrix, p_step)
-    
-    # Writing to file.
-    np.savetxt("phase_data.dat", phase_matrix, fmt='%1.5f', delimiter=' ')
-    np.savetxt("var_data.dat", var_matrix, fmt='%1.5f', delimiter=' ')
-
+                        psis.append(simulation.get_infected_frac())
+            var_array[int(p1*(p1s.size-1))] = simulation.get_infected_var(psis) / \
+                (simulation.size[0] * simulation.size[1])
+        # Plotting.
+        simulation.plot_variance(p1s, var_array)
+            
 main()
 
 
